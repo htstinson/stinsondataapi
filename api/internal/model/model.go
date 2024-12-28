d5a12804f12021bd9097539e7e86ec7afe9f2f3a
Tom Stinson <tom.stinson@stinsondata.com>
2024-12-28 11:49:37 -0600

    update

--- api/internal/model/model.go
+++ api/internal/model/model.go
@@ -5,6 +5,14 @@ import (
 	"time"
 )
 
+type RDSLogin struct {
+	Username              string `json:"username"`
+	Password              string `json:"password"`
+	Host                  string `json:"host"`
+	Port                  int    `json:"port"`
+	DdbInstanceIdentifier string `json:"dbInstanceIdentifier"`
+}
+
 // Item
 type Item struct {
 	ID        string    `json:"id"`
