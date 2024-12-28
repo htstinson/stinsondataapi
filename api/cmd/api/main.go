9b94259a7025e0d49b0d873fc94c8c740de00108
Tom Stinson <tom.stinson@stinsondata.com>
2024-12-28 12:39:12 -0600

    update

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -144,20 +144,7 @@ func main() {
 		Addr:    ":443",
 		Handler: router,
 		TLSConfig: &tls.Config{
-			MinVersion: tls.VersionTLS12,
-			CurvePreferences: []tls.CurveID{
-				tls.X25519,
-				tls.CurveP256,
-				tls.CurveP384,
-			},
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			},
+			MinVersion: tls.VersionTLS13,
 		},
 	}
 
