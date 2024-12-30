064b7a4905da2417c88ea59aebc4a4d8e94c55a6
Tom Stinson <tom.stinson@stinsondata.com>
2024-12-29 18:27:54 -0600

    update

--- api/internal/middleware/middleware.go
+++ api/internal/middleware/middleware.go
@@ -51,7 +51,7 @@ func SecurityHeaders(next http.Handler) http.Handler {
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Type, Authorization")
 
