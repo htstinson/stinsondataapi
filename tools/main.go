ca44e272f1296f42dab2404d2536230cb4018daa
Tom Stinson <tom.stinson@stinsondata.com>
2024-12-31 11:16:56 -0600

    update

--- tools/main.go
+++ tools/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/htstinson/stinsondataapi/tree/main/api/internal/salesforce"
+)
 
 func main() {
-	fmt.Println("hello")
+	logger := log.New(os.Stdout, "[API] ", log.LstdFlags)
+
+	logger.Println("initialize salesforce")
+	sf, err := salesforce.New()
+	if err != nil {
+		logger.Println(err.Error())
+		return
+	}
+
 }
