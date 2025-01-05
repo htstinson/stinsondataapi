f250d02b3dff6655884f2de6f60fc6065bae6d13
Tom Stinson <tom.stinson@stinsondata.com>
2025-01-05 15:58:45 -0600

    update

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -108,7 +108,7 @@ func main() {
 	protected.HandleFunc("/accounts", sf.Handler.ListAccounts).Methods("GET", "OPTIONS")
 
 	protected.HandleFunc("/contacts", sf.Handler.ListContacts).Methods("GET", "OPTIONS")
-	protected.HandleFunc("/contacts/{accountid", sf.Handler.ListContacts).Methods("GET", "OPTIONS")
+	protected.HandleFunc("/contacts/{accountid}", sf.Handler.ListContacts).Methods("GET", "OPTIONS")
 	protected.HandleFunc("/contact/{contactid}", sf.Handler.GetContactById).Methods("GET", "OPTIONS")
 
 	protected.HandleFunc("/users", h.CreateUser).Methods("POST", "OPTIONS")
