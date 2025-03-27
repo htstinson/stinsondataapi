820abc95ecc0a410147c75995f1223426e0b4f15
Tom Stinson <tom.stinson@stinsondata.com>
2025-03-27 12:55:29 -0500

    update

--- api/internal/middleware/middleware.go
+++ api/internal/middleware/middleware.go
@@ -16,11 +16,12 @@ func Logger(logger *log.Logger) func(http.Handler) http.Handler {
 			start := time.Now()
 			next.ServeHTTP(w, r)
 			fmt.Printf(
-				"%s %s %s %s\n",
+				"[%v] %s %s %s %s\n",
 				r.Method,
 				r.URL.Path,
 				r.RemoteAddr,
 				time.Since(start),
+				time.Now().Format(time.RFC3339),
 			)
 		})
 	}
