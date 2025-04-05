fa7e9a74fe310fa9ab47f4e2d4e752e1f8b7d8d4
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-04 22:53:25 -0500

    add parser

--- api/pkg/database/database.go
+++ api/pkg/database/database.go
@@ -308,13 +308,10 @@ func (d *Database) GetBlocked(ctx context.Context, id string) (*model.Blocked, e
 }
 
 func (d *Database) CreateBlocked(ctx context.Context, blocked model.Blocked) (*model.Blocked, error) {
-	fmt.Println("d CreateBlocked")
-
 	blocked.CreatedAt = time.Now()
 
 	_, err := d.GetBlockedByIP(ctx, blocked.IP)
 	if err == nil {
-		fmt.Printf("duplicate IP %s\n", blocked.IP)
 		return nil, errors.New("duplicate")
 	}
 
