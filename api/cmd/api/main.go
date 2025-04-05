68ef27b1e19f887c693c2f66dfce825c807a03be
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-04 22:36:14 -0500

    add parser

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -96,10 +96,10 @@ func main() {
 			newblocked, err := db.CreateBlocked(ctx, *blocked)
 			if err != nil {
 				if err.Error() == "duplicate" {
-					fmt.Printf(`[%v] [main] %v blocked IP %s already exists.\n`, time.Now().Format(time.RFC3339), k, newblocked.IP)
+					fmt.Printf(`[%v] [main] %v blocked IP %s already exists.\\n`, time.Now().Format(time.RFC3339), k, newblocked.IP)
 				}
 			} else {
-				fmt.Printf(`[%v] [main] %v Created blocked IP %s.\n`, time.Now().Format(time.RFC3339), k, newblocked.IP)
+				fmt.Printf(`[%v] [main] %v Created blocked IP %s.\\n`, time.Now().Format(time.RFC3339), k, newblocked.IP)
 			}
 			time.Sleep(100 * time.Millisecond)
 		}
