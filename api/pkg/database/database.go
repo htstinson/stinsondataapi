b66b81482fee7d1ad8eb2d997772e30c4001361a
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-05 21:11:59 -0500

    add parser

--- api/pkg/database/database.go
+++ api/pkg/database/database.go
@@ -243,9 +243,9 @@ func (d *Database) RowCount(tablename string) (int, error) {
 // Admin - Blocked
 func (d *Database) SelectBlocked(ctx context.Context, limit, offset int, sort string, order string) ([]model.Blocked, error) {
 
-	q := fmt.Sprintf("SELECT id, ip, notes, created_at FROM blocked ORDER BY $1 %s LIMIT $2 OFFSET $3", strings.ToUpper(order))
+	q := fmt.Sprintf("SELECT id, ip, notes, created_at FROM blocked ORDER BY %s %s LIMIT $2 OFFSET $3", sort, strings.ToUpper(order))
 
-	rows, err := d.db.QueryContext(ctx, q, sort, limit, offset)
+	rows, err := d.db.QueryContext(ctx, q, limit, offset)
 
 	if err != nil {
 		fmt.Printf("[%v] [database][SelectBlocked] error: %s.\n", time.Now().Format(time.RFC3339), err.Error())
