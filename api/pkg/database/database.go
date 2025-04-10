3909d368decf7bde120174a457993baaa2e257b4
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-10 14:36:52 -0500

    update

--- api/pkg/database/database.go
+++ api/pkg/database/database.go
@@ -490,12 +490,11 @@ func (d *Database) DeleteUser(ctx context.Context, id string) error {
 
 func (d *Database) UpdateUser(ctx context.Context, user *model.User) error {
 
-	query := `UPDATE users SET username = $1, ip_address = $3 WHERE id = $2`
+	query := `UPDATE users SET username = $1, ip_address = $2 WHERE id = $3`
 
 	_, err := d.db.ExecContext(ctx, query, user.Username, user.IP_address, user.ID)
 
 	return err
-
 }
 
 //Roles
