fab6ee6c5ae17cd6fb1e98fa0ccd97e48c512e5a
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-10 14:27:27 -0500

    update

--- api/internal/handler/user.go
+++ api/internal/handler/user.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -32,6 +33,7 @@ func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("h UpdateUser")
 	vars := mux.Vars(r)
 	id := vars["id"]
 
