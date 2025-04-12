a8020a4b180ddf50998f1e78067f4e31fed9459f
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-12 07:43:56 -0500

    update

--- api/internal/handler/role.go
+++ api/internal/handler/role.go
@@ -1,13 +1,14 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	common "github.com/htstinson/stinsondataapi/api/commonweb"
 )
 
 func (h *Handler) SelectRoles(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("h SelectRoles")
 	ctx := r.Context()
 	customers, err := h.db.SelectRoles(ctx, 100, 0)
 	if err != nil {
