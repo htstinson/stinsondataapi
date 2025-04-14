088e0199a2ccfdd23725e7c2c8b68d9f24a2cd58
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-14 08:39:49 -0500

    update

--- api/internal/model/rolepermission.go
+++ api/internal/model/rolepermission.go
@@ -7,3 +7,14 @@ type Role_Permission struct {
 	Permission_Id string    `json:"permission_id"`
 	CreatedAt     time.Time `json:"created_at"`
 }
+
+type Role_Permission_View struct {
+	Role_Id         string    `json:"role_id"`
+	Role_Name       string    `json:"role_name"`
+	Permission_Id   string    `json:"permission_id"`
+	Permission_Name string    `json:"permission_name"`
+	Object_Id       string    `json:"object_id"`
+	Object_Name     string    `json:"object_name"`
+	Object_Type     string    `json:"object_type"`
+	CreatedAt       time.Time `json:"created_at"`
+}
--- api/pkg/database/rolepermissions.go
+++ api/pkg/database/rolepermissions.go
@@ -7,10 +7,11 @@ import (
 	"github.com/htstinson/stinsondataapi/api/internal/model"
 )
 
-func (d *Database) SelectRolePermissions(ctx context.Context, limit, offset int) ([]model.Role_Permission, error) {
+func (d *Database) SelectRolePermissions(ctx context.Context, limit, offset int) ([]model.Role_Permission_View, error) {
 	fmt.Println("database.go SelectRolePermission()")
+
 	rows, err := d.db.QueryContext(ctx,
-		"SELECT role_id, permission_id, created_at FROM Role_permissions ORDER BY Role_id ASC LIMIT $1 OFFSET $2",
+		"SELECT role_id, role_name, permission_id, permission_name, object_id, object_name, object_type, created_at FROM role_permissions_view ORDER BY Role_name, permission_name, object_name ASC LIMIT $1 OFFSET $2",
 		limit, offset,
 	)
 	if err != nil {
@@ -19,15 +20,18 @@ func (d *Database) SelectRolePermissions(ctx context.Context, limit, offset int)
 	}
 	defer rows.Close()
 
-	var role_permissions []model.Role_Permission
+	var role_permissions_view []model.Role_Permission_View
 	for rows.Next() {
-		var role_permission model.Role_Permission
-		if err := rows.Scan(&role_permission.Role_Id, &role_permission.Permission_Id, &role_permission.CreatedAt); err != nil {
+		var role_permission_view model.Role_Permission_View
+		if err := rows.Scan(&role_permission_view.Role_Id, &role_permission_view.Role_Name,
+			&role_permission_view.Permission_Id, &role_permission_view.Permission_Name,
+			&role_permission_view.Object_Id, &role_permission_view.Object_Name, &role_permission_view.Object_Type,
+			&role_permission_view.CreatedAt); err != nil {
 			fmt.Println(err.Error())
 			return nil, fmt.Errorf("error scanning user_permission: %w", err)
 		}
 
-		role_permissions = append(role_permissions, role_permission)
+		role_permissions_view = append(role_permissions_view, role_permission_view)
 	}
-	return role_permissions, nil
+	return role_permissions_view, nil
 }
