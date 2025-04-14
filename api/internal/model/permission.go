147a8af4feaac4f159c6cbdd8266c9ad62d7991b
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-14 07:11:58 -0500

    update

--- api/internal/model/permission.go
+++ api/internal/model/permission.go
@@ -7,5 +7,6 @@ type Permission struct {
 	Id          string    `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
+	Object_Id   string    `json:"object_id"`
 	CreatedAt   time.Time `json:"created_at"`
 }
