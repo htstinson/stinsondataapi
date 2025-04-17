d083b120b75ad70f27ab55e64275026ba082974d
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-17 16:34:12 -0500

    update

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -139,7 +139,7 @@ func main() {
 	protected.HandleFunc("/users/{id}", h.GetUser).Methods("GET")
 	protected.HandleFunc("/users", h.SelectUsers).Methods("GET", "OPTIONS")
 	protected.HandleFunc("/users/roles", h.SelectUserRoles).Methods("GET", "OPTIONS")
-	protected.HandleFunc("/profile", h.GetUser).Methods("POST", "OPTIONS")
+	protected.HandleFunc("/profile", h.GetUser).Methods("GET", "OPTIONS")
 
 	// Customer
 	protected.HandleFunc("/customers", h.CreateCustomer).Methods("POST", "OPTIONS")
