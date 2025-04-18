0d15fa6cad39d8725b73f1dc35a77317c0955825
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-18 16:33:42 -0500

    update

--- api/pkg/database/usercustomer.go
+++ api/pkg/database/usercustomer.go
@@ -10,7 +10,7 @@ import (
 func (d *Database) SelectUserCustomerView(ctx context.Context, limit, offset int) ([]model.User_Customer_View, error) {
 	fmt.Println("database.go SelectUserCustomerView()")
 	rows, err := d.db.QueryContext(ctx,
-		"SELECT id, user_id, customer_id, user_username, customer_name, assigned_at FROM user_customer_view ORDER BY user_username, customer_name ASC LIMIT $1 OFFSET $2",
+		"SELECT id, user_id, customer_id, user_username, customer_name FROM user_customer_view ORDER BY user_username, customer_name ASC LIMIT $1 OFFSET $2",
 		limit, offset,
 	)
 	if err != nil {
@@ -22,7 +22,7 @@ func (d *Database) SelectUserCustomerView(ctx context.Context, limit, offset int
 	var user_customer_views []model.User_Customer_View
 	for rows.Next() {
 		var user_customer_view model.User_Customer_View
-		if err := rows.Scan(&user_customer_view.Id, &user_customer_view.User_ID, &user_customer_view.Customer_Id, &user_customer_view.User_Username, &user_customer_view.Customer_Name, &user_customer_view.Assignedd_At); err != nil {
+		if err := rows.Scan(&user_customer_view.Id, &user_customer_view.User_ID, &user_customer_view.Customer_Id, &user_customer_view.User_Username, &user_customer_view.Customer_Name); err != nil {
 			fmt.Println(err.Error())
 			return nil, fmt.Errorf("error scanning user_permission: %w", err)
 		}
