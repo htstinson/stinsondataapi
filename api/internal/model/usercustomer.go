75c75caec775165dd9a11d4415d27a604533b9ff
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-19 08:38:23 -0500

    update

--- api/internal/model/usercustomer.go
+++ api/internal/model/usercustomer.go
@@ -4,10 +4,10 @@ import "time"
 
 // User_Customrer
 type User_Customer struct {
-	Id           string    `json:"id"`
-	User_ID      string    `json:"user_id"`
-	Customer_Id  string    `json:"customer_id"`
-	Assignedd_At time.Time `json:"assigned_at"`
+	Id          string    `json:"id"`
+	User_ID     string    `json:"user_id"`
+	Customer_Id string    `json:"customer_id"`
+	Assigned_At time.Time `json:"assigned_at"`
 }
 
 // User_Customrer_View
