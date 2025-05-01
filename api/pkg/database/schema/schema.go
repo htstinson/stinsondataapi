68c393c9e9b39e9da240a496893d79e29e23986b
Tom Stinson <tom.stinson@stinsondata.com>
2025-04-30 20:30:20 -0500

    update

--- api/pkg/database/schema/schema.go
+++ api/pkg/database/schema/schema.go
@@ -344,13 +344,10 @@ func (schema *Schema) createStructures(ctx context.Context, sequences []string, 
 		}
 		pkRows.Close()
 
+		triggerSQL := fmt.Sprintf("CREATE TRIGGER update_profile_modified BEFORE UPDATE ON %s.%s FOR EACH ROW EXECUTE FUNCTION update_modified_column();", schema.ToSchemaName, tableName)
+
 		// Apply trigger
-		_, err = tx.ExecContext(ctx, `
-			CREATE TRIGGER update_profile_modified 
-			BEFORE UPDATE ON $1.$2 
-			FOR EACH ROW 
-			EXECUTE FUNCTION update_modified_column();
-		`, schema.ToSchemaName, tableName)
+		_, err = tx.ExecContext(ctx, triggerSQL, schema.ToSchemaName, tableName)
 
 		if err != nil {
 			return fmt.Errorf("failed to create trigger: %w", err)
