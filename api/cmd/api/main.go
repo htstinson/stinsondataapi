04cb02c4bd2c834fc1c51564dc50abf1314d66eb
Tom Stinson <tom.stinson@stinsondata.com>
2025-12-27 18:18:44 -0600

    added subscribercustomers api endpoint

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -160,6 +160,7 @@ func main() {
 
 	// Subscriber - Customer
 	protected.HandleFunc("/subscriber/customers", h.SelectCustomers).Methods("POST", "OPTIONS")
+	protected.HandleFunc("/subscribercustomers", h.SelectSubscriberCustomers).Methods("POST", "OPTIONS")
 	protected.HandleFunc("/subscriber/customer", h.CreateCustomer).Methods("POST", "OPTIONS")
 	protected.HandleFunc("/subscriber/customerd", h.DeleteCustomer).Methods("POST", "OPTIONS")
 	protected.HandleFunc("/subscriber/customer", h.UpdateCustomer).Methods("PUT", "OPTIONS")
