3bc4251288563804fad01d312d65e5b187d40b59
Tom Stinson <tom.stinson@stinsondata.com>
2025-12-30 10:14:08 -0600

    update

---
+++ api/internal/model/calibrate/search_definitions.go
@@ -0,0 +1,12 @@
+package calibrate
+
+import "time"
+
+type SearchDefinition struct {
+	Id                string    `json:"id"`
+	CreatedAt         time.Time `json:"created_at"`
+	ModifiedAt        time.Time `json:"modified_at"`
+	SearchName        string    `json:"search_name"`
+	SearchDefinition  string    `json:"search_definition"`
+	SearchDescription string    `json:"search_description"`
+}
---
+++ api/pkg/database/calibrate/search_definition.go
@@ -0,0 +1,40 @@
+package calibrate
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/htstinson/stinsondataapi/api/internal/model"
+)
+
+func (d *Database) SelectSearchDefinitions(ctx context.Context, customer model.Customer, limit, offset int) ([]model.Contact, error) {
+
+	fmt.Println("d SelectContacts")
+
+	query := fmt.Sprintf(`SELECT id, parent_id, lastname, firstname, created_at FROM %s.contacts 
+	WHERE parent_id = '%s' ORDER BY lastname,firstname ASC LIMIT $1 OFFSET $2`, customer.Schema_Name, customer.Id)
+
+	rows, err := d.DB.QueryContext(ctx,
+		query,
+		limit, offset,
+	)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, fmt.Errorf("error listing contacts: %w", err)
+	}
+	defer rows.Close()
+
+	var contacts []model.Contact
+	for rows.Next() {
+		var contact model.Contact
+		if err := rows.Scan(&contact.Id, &contact.ParentId, &contact.LastName, &contact.FirstName, &contact.CreatedAt); err != nil {
+			fmt.Println(err.Error())
+			return nil, fmt.Errorf("error scanning contact: %w", err)
+		}
+
+		contact.Schema_Name_ = customer.Schema_Name
+
+		contacts = append(contacts, contact)
+	}
+	return contacts, nil
+}
