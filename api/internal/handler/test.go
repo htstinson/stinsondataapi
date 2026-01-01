0eb7e2f8b640f8a40480fffedfb36fc84c0a5e65
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-01 15:20:59 -0600

    udpate

--- api/internal/handler/test.go
+++ api/internal/handler/test.go
@@ -81,7 +81,7 @@ func (h *Handler) Test(w http.ResponseWriter, r *http.Request) {
 	daterange := searcher.DateRangeConfig{
 		Type:      "d",        // "d", "w", "m", "y"
 		Value:     100,        //int
-		StartDate: "20251231", //YYYYMMDD format
+		StartDate: "20251201", //YYYYMMDD format
 		EndDate:   "20261010", //YYYYMMDD format
 	}
 
