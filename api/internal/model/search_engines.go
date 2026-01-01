b480cebbd5df338721ae4179c6cd8400455a2d78
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-01 11:43:44 -0600

    update

--- api/internal/model/search_engines.go
+++ api/internal/model/search_engines.go
@@ -1,16 +1,15 @@
 package model
 
 import (
-	"database/sql"
 	"time"
 )
 
 type SearchEngine struct {
-	Id             string         `json:"id"`
-	ParentId       string         `json:"parent_id"`
-	CreatedAt      time.Time      `json:"created_at"`
-	ModifiedAt     time.Time      `json:"modified_at"`
-	Name           string         `json:"name"`
-	SearchEngineId string         `json:"search_engine_id"`
-	Comment        sql.NullString `json:"comment"`
+	Id             string    `json:"id"`
+	ParentId       string    `json:"parent_id"`
+	CreatedAt      time.Time `json:"created_at"`
+	ModifiedAt     time.Time `json:"modified_at"`
+	Name           string    `json:"name"`
+	SearchEngineId string    `json:"search_engine_id"`
+	Comment        *string   `json:"comment"`
 }
