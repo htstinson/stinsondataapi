4eb11a617c90f5e1859733b8fc950f14c8fb2aa0
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-01 21:00:27 -0600

    udpate

--- api/internal/handler/test.go
+++ api/internal/handler/test.go
@@ -80,7 +80,7 @@ func (h *Handler) Test(w http.ResponseWriter, r *http.Request) {
 	daterange := searcher.DateRangeConfig{
 		Type:      "custom",
 		Value:     1000,
-		StartDate: "2025-09-01",
+		StartDate: "2025-07-01",
 		EndDate:   "2025-12-31",
 	}
 
