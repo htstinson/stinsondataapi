4647cd11796bf240d19e0aa1ae918da22fa82397
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-02 16:50:28 -0600

    udpate

--- api/pkg/database/search_definition.go
+++ api/pkg/database/search_definition.go
@@ -11,7 +11,8 @@ func (d *Database) SelectSearchDefinitions(ctx context.Context, subscriber model
 
 	fmt.Println("d SelectSearchDefinitions")
 
-	query := fmt.Sprintf(`SELECT id, created_at, modified_at, name, comment, query, exact_match, max_results, sort_by_date, start_date, end_date, search_type FROM %s.calibrate_search_definition 
+	query := fmt.Sprintf(`SELECT id, created_at, modified_at, name, comment, query, exact_match, max_results, sort_by_date, start_date, end_date, 
+	search_type, subscriber_id FROM %s.calibrate_search_definition 
 	ORDER BY name ASC LIMIT $1 OFFSET $2`, subscriber.Schema_Name)
 
 	rows, err := d.DB.QueryContext(ctx,
@@ -27,7 +28,9 @@ func (d *Database) SelectSearchDefinitions(ctx context.Context, subscriber model
 	var searchdefinitions []model.SearchDefinition
 	for rows.Next() {
 		var searchdefinition model.SearchDefinition
-		if err := rows.Scan(&searchdefinition.Id, &searchdefinition.CreatedAt, &searchdefinition.ModifiedAt, &searchdefinition.Name, &searchdefinition.Comment, &searchdefinition.Query, &searchdefinition.ExactMatch, &searchdefinition.MaxResults, &searchdefinition.SortByDate, &searchdefinition.StartDate, &searchdefinition.EndDate, &searchdefinition.SearchType); err != nil {
+		if err := rows.Scan(&searchdefinition.Id, &searchdefinition.CreatedAt, &searchdefinition.ModifiedAt, &searchdefinition.Name, &searchdefinition.Comment,
+			&searchdefinition.Query, &searchdefinition.ExactMatch, &searchdefinition.MaxResults, &searchdefinition.SortByDate, &searchdefinition.StartDate,
+			&searchdefinition.EndDate, &searchdefinition.SearchType, &searchdefinition.SubscriberId); err != nil {
 			fmt.Println(err.Error())
 			return nil, fmt.Errorf("error scanning search_definition: %w", err)
 		}
