fab7f1e72e81e4d1c9f4b58bb75c40371e9957be
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-02 16:06:35 -0600

    udpate

--- api/pkg/database/search_definition_engines.go
+++ api/pkg/database/search_definition_engines.go
@@ -11,7 +11,7 @@ func (d *Database) SelectSearchDefinitionEnginesView(ctx context.Context, subscr
 	fmt.Println("d SelectSearchDefinitionEnginesView")
 
 	query := fmt.Sprintf(`SELECT id, created_at, modified_at, search_engine_Id, search_engine_name, 
-	search_definition_name, search_query, engine_id, definition_id FROM %s.calibrate_search_engines ORDER BY name ASC LIMIT $1 OFFSET $2`, subscriber.Schema_Name)
+	search_definition_name, search_query, engine_id, definition_id FROM %s.search_definition_engines_view ORDER BY name ASC LIMIT $1 OFFSET $2`, subscriber.Schema_Name)
 
 	fmt.Println(query)
 
