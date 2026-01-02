5619db13aa7d953907d5a6881e9c1e32b58f0135
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-02 10:45:58 -0600

    udpate

--- api/internal/handler/search_engines.go
+++ api/internal/handler/search_engines.go
@@ -16,13 +16,16 @@ func (h *Handler) ListSearchEngines(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	subscriber_Id := vars["subscriber_id"]
 
-	fmt.Println(subscriber_Id)
-
 	subscriber, err := h.db.GetSubscriber(ctx, subscriber_Id)
+	if err != nil {
+		fmt.Println(err.Error())
+		common.RespondError(w, http.StatusInternalServerError, "Failed to select subcriber")
+		return
+	}
 
 	search_engines, err := h.db.SelectSearchEngines(ctx, *subscriber, 100, 0)
 	if err != nil {
-		common.RespondError(w, http.StatusInternalServerError, "Failed to select user_subscriber_view")
+		common.RespondError(w, http.StatusInternalServerError, "Failed to select search engines")
 		return
 	}
 
