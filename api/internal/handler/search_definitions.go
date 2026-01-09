48a214e4370c84b685390f3f1791597bdfb0e036
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-09 16:11:24 -0600

    update

--- api/internal/handler/search_definitions.go
+++ api/internal/handler/search_definitions.go
@@ -32,3 +32,34 @@ func (h *Handler) SelectSearchDefinitions(w http.ResponseWriter, r *http.Request
 	common.RespondJSON(w, http.StatusOK, results)
 
 }
+
+func (h *Handler) DeleteSearchDefinition(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("h DeleteSearchDefinition")
+
+	vars := mux.Vars(r)
+	subscriber_id := vars["subscriber_id"]
+	search_definition_id := vars["search_definition_id"]
+
+	ctx := r.Context()
+
+	subscriber, err := h.db.GetSubscriber(ctx, subscriber_id)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	search_engine, err := h.db.GetSearchDefinition(ctx, *subscriber, search_definition_id, 1, 0)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	err = h.db.DeleteSearchDefinition(ctx, subscriber, search_definition_id)
+	if err != nil {
+		common.RespondError(w, http.StatusNotFound, "Error deleting Search Engine")
+		return
+	}
+
+	common.RespondJSON(w, http.StatusOK, search_engine)
+
+}
