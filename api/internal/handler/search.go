f8b941f065810dfe64c23986af73f097b3d6ff22
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-10 14:34:20 -0600

    update

--- api/internal/handler/search.go
+++ api/internal/handler/search.go
@@ -160,7 +160,7 @@ func (h *Handler) Search(w http.ResponseWriter, r *http.Request) {
 						fmt.Println(err.Error())
 					}
 					count++
-					fmt.Println(calbrate_search_result.Published)
+					fmt.Println(calbrate_search_result.Published.Format(time.RFC3339))
 				}
 				fmt.Println("Total Results", count)
 				fmt.Println("------------------------------------------------------------------------------------------", v.Id)
