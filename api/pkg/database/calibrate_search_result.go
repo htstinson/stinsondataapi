fd26e90e57b890ba264914dbabd9420a63f70564
Tom Stinson <tom.stinson@stinsondata.com>
2026-01-10 14:25:22 -0600

    update

--- api/pkg/database/calibrate_search_result.go
+++ api/pkg/database/calibrate_search_result.go
@@ -39,7 +39,7 @@ func (d *Database) SelectSearchResultView(ctx context.Context, subscriber model.
 	query := fmt.Sprintf(`SELECT result_id, link, snippet, title, search_time, result_created_at, subscriber_id,
 	search_definition_id, search_definition_name, query, search_definition_comment, exact_match, max_results, sort_by_date,
 	start_date, end_date, search_type, search_engine_id, search_engine_name, search_engine_identifier, search_engine_comment,
-	search_definition_engine_id  
+	search_definition_engine_id, published  
 	FROM %s.%s WHERE search_definition_engine_id = $1`, schema_name, table)
 
 	rows, err := d.DB.QueryContext(ctx, query, sde)
@@ -53,7 +53,7 @@ func (d *Database) SelectSearchResultView(ctx context.Context, subscriber model.
 		if err := rows.Scan(&item.ResultId, &item.Link, &item.Snippet, &item.Title, &item.SearchTime, &item.ResultCreatedAt, &item.SubscriberId,
 			&item.SearchDefinitionId, &item.SearchDefinitionName, &item.Query, &item.SearchDefinitionComment, &item.ExactMatch, &item.MaxResults, &item.SortByDate,
 			&item.StartDate, &item.EndDate, &item.SearchType, &item.SearchEngineId, &item.SearchEngineName, &item.SearchEngineIdentifier, &item.SearchEngineComment,
-			&item.SearchDefinitionEngineID); err != nil {
+			&item.SearchDefinitionEngineID, &item.Published); err != nil {
 			return nil, fmt.Errorf("error scanning item: %w", err)
 		}
 		items = append(items, item)
