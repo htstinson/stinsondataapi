1708c7703f72c009c1c82ff2a4f736761caf0d82
Tom Stinson <tom.stinson@stinsondata.com>
2026-03-30 22:40:04 -0500

    update

--- api/internal/handler/subscriber.go
+++ api/internal/handler/subscriber.go
@@ -58,34 +58,31 @@ func (h *Handler) CreateSubscriber(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) GetSubscriberProfile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("h GetSubscriberProfile")
 
-	/*
-		var subcriber *model.Subscriber
-		if err := json.NewDecoder(r.Body).Decode(&subcriber); err != nil {
-			common.RespondError(w, http.StatusBadRequest, "Invalid request payload")
-			return
-		}
-		defer r.Body.Close()
+	var subcriber *model.Subscriber
+	if err := json.NewDecoder(r.Body).Decode(&subcriber); err != nil {
+		common.RespondError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
 
-		ctx := r.Context()
+	ctx := r.Context()
 
-		subcriber, err := h.db.GetSubscriber(ctx, subcriber.Id)
-		if err != nil {
-			fmt.Println(err.Error())
-			common.RespondError(w, http.StatusInternalServerError, "Failed to get subscriber")
-			return
-		}
+	subcriber, err := h.db.GetSubscriber(ctx, subcriber.Id)
+	if err != nil {
+		fmt.Println(err.Error())
+		common.RespondError(w, http.StatusInternalServerError, "Failed to get subscriber")
+		return
+	}
 
-		profile, err := h.db.GetProfileByParent(ctx, subcriber)
-		if err != nil {
-			fmt.Println(err.Error())
-			common.RespondError(w, http.StatusInternalServerError, "Failed to get profile")
-			return
-		}
+	profile, err := h.db.GetProfileByParent(ctx, subcriber)
+	if err != nil {
+		fmt.Println(err.Error())
+		common.RespondError(w, http.StatusInternalServerError, "Failed to get profile")
+		return
+	}
 
-		common.RespondJSON(w, http.StatusOK, profile)
-	*/
-	common.RespondJSON(w, http.StatusOK, nil)
-
+	println(profile.Legal_Name)
+	common.RespondJSON(w, http.StatusOK, profile)
 }
 
 func (h *Handler) UpdateSubscriber(w http.ResponseWriter, r *http.Request) {
