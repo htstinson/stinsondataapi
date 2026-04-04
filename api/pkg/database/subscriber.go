6716d898736cb710c2e46ce2013803c8676a7034
Tom Stinson <tom.stinson@stinsondata.com>
2026-04-04 17:30:48 -0500

    update

--- api/pkg/database/subscriber.go
+++ api/pkg/database/subscriber.go
@@ -13,6 +13,7 @@ import (
 // subscribers
 func (d *Database) GetSubscriber(ctx context.Context, id string) (*model.Subscriber, error) {
 	fmt.Println("d GetSubscriber")
+	fmt.Println("id", id)
 
 	var subscriber model.Subscriber
 
