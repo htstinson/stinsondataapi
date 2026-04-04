e079ae4982d05655d26762c7dda9c9a15e57f083
Tom Stinson <tom.stinson@stinsondata.com>
2026-04-04 00:03:45 -0500

    update

--- api/internal/model/subscriber_address.go
+++ api/internal/model/subscriber_address.go
@@ -8,12 +8,12 @@ type Address struct {
 	SubscriberId string    `json:"subscriber_id"`
 	CreatedAt    time.Time `json:"created_at"`
 	ModifiedAt   time.Time `json:"modified_at"`
-	AddressType  string    `json:"address_type"`
-	AddressUse   string    `json:"address_use"`
-	Street1      string    `json:"street1"`
-	Street2      string    `json:"street2"`
-	POBox        string    `json:"po_box"`
-	City         string    `json:"city"`
-	State        string    `json:"state"`
-	Zip          string    `json:"zip"`
+	AddressType  *string   `json:"address_type"`
+	AddressUse   *string   `json:"address_use"`
+	Street1      *string   `json:"street1"`
+	Street2      *string   `json:"street2"`
+	POBox        *string   `json:"po_box"`
+	City         *string   `json:"city"`
+	State        *string   `json:"state"`
+	Zip          *string   `json:"zip"`
 }
