896e9c517c4d7ea0cf6d996d81f7121bcad6173c
Tom Stinson <tom.stinson@stinsondata.com>
2026-04-05 19:59:37 -0500

    update

--- api/internal/model/profile.go
+++ api/internal/model/profile.go
@@ -6,22 +6,22 @@ import "time"
 type Profile struct {
 	Id             string    `json:"id"`
 	ParentId       string    `json:"parentid"`
-	Legal_Name     string    `json:"legal_name"`
-	Phone          string    `json:"phone"`
-	Fax            string    `json:"fax"`
-	Email          string    `json:"email"`
-	Website        string    `json:"website"`
-	LinkedIn       string    `json:"linkedin"`
-	Facebook       string    `json:"facebook"`
-	Instagram      string    `json:"instagram"`
-	X              string    `json:"x"`
-	YouTube        string    `json:"youtube"`
-	Pinterest      string    `json:"pinterest"`
-	GoogleBusiness string    `json:"google_business"`
-	Yelp           string    `json:"yelp"`
-	GlassDoor      string    `json:"glassdoor"`
-	Github         string    `json:"github"`
-	NextDoor       string    `json:"nextdoor"`
+	Legal_Name     *string   `json:"legal_name"`
+	Phone          *string   `json:"phone"`
+	Fax            *string   `json:"fax"`
+	Email          *string   `json:"email"`
+	Website        *string   `json:"website"`
+	LinkedIn       *string   `json:"linkedin"`
+	Facebook       *string   `json:"facebook"`
+	Instagram      *string   `json:"instagram"`
+	X              *string   `json:"x"`
+	YouTube        *string   `json:"youtube"`
+	Pinterest      *string   `json:"pinterest"`
+	GoogleBusiness *string   `json:"google_business"`
+	Yelp           *string   `json:"yelp"`
+	GlassDoor      *string   `json:"glassdoor"`
+	Github         *string   `json:"github"`
+	NextDoor       *string   `json:"nextdoor"`
 	CreatedAt      time.Time `json:"created_at"`
 	ModifiedAt     time.Time `json:"modified_at"`
 }
