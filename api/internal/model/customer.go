56dadac4b666a2ab12305591608e371dd4695987
Tom Stinson <tom.stinson@stinsondata.com>
2026-05-02 07:22:44 -0500

    update

--- api/internal/model/customer.go
+++ api/internal/model/customer.go
@@ -6,7 +6,8 @@ import "time"
 type Customer struct {
 	Id            string    `json:"id"`
 	Name          string    `json:"name"`
-	Subscriber_ID string    `json:"subscriber_id"`
+	Parent_Id     string    `json:"parent_id"`
+	Subscriber_Id string    `json:"subscriber_id"`
 	Schema_Name   string    `json:"schema_name"`
 	CreatedAt     time.Time `json:"created_at"`
 }
