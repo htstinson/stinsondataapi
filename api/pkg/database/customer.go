6e69c87f19c0da47522d7c24706bc67698354033
Tom Stinson <tom.stinson@stinsondata.com>
2026-05-03 16:30:49 -0500

    update

--- api/pkg/database/customer.go
+++ api/pkg/database/customer.go
@@ -50,7 +50,7 @@ func (d *Database) SelectCustomers(ctx context.Context, subscriber model.Subscri
 	return customers, total, nil
 }
 
-func (d *Database) CreateCustomer(ctx context.Context, customer *model.Customer, profile *model.Profile, subscriber *model.Subscriber) (*model.Customer, error) {
+func (d *Database) CreateCustomer(ctx context.Context, customer model.Customer, profile *model.Profile, subscriber *model.Subscriber) (*model.Customer, error) {
 	fmt.Println("d CreateCustomer")
 
 	query := fmt.Sprintf(`INSERT INTO %s.customers (id, name, parent_id) VALUES ($1, $2, $3)`, customer.Schema_Name)
@@ -70,7 +70,7 @@ func (d *Database) CreateCustomer(ctx context.Context, customer *model.Customer,
 		return nil, fmt.Errorf("error creating customer: %w", err)
 	}
 
-	return customer, nil
+	return &customer, nil
 
 }
 
