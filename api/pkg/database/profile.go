6d2c523ff4ba05777fc4778cd4420df92aba2eba
Tom Stinson <tom.stinson@stinsondata.com>
2026-05-04 16:56:04 -0500

    update

--- api/pkg/database/profile.go
+++ api/pkg/database/profile.go
@@ -122,21 +122,19 @@ func (d *Database) UpdateProfile(ctx context.Context, subscriber *model.Subscrib
 			profile.YouTube, profile.Pinterest, profile.GoogleBusiness, profile.Yelp, profile.GlassDoor, profile.Github, profile.NextDoor, profile.Bizapedia, profile.Id)
 	*/
 
-	if subscriber != nil {
-		fmt.Println(subscriber.Schema_Name)
-	} else {
+	_, err := fmt.Println(subscriber.Schema_Name)
+	if err != nil {
 		fmt.Println("subscriber is nil")
 	}
 
-	if profile != nil {
-		fmt.Println(profile.Legal_Name)
-	} else {
+	_, err = fmt.Println(profile.Legal_Name)
+	if err != nil {
 		fmt.Println("Profile is nil")
 	}
 
 	query := fmt.Sprintf(`UPDATE %s.profile SET parent_id=$2, legal_name=$3 WHERE id=$1`, subscriber.Schema_Name)
 
-	_, err := d.DB.ExecContext(ctx, query, profile.Id, profile.Subscriber_Id, *profile.Legal_Name)
+	_, err = d.DB.ExecContext(ctx, query, profile.Id, profile.Subscriber_Id, *profile.Legal_Name)
 
 	return err
 }
