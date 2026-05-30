3717939afbccaffdf789cf322ac9be9683b4218a
Tom Stinson <tom.stinson@stinsondata.com>
2026-05-29 21:17:36 -0500

    update

---
+++ api/commonweb/gmail.go
@@ -0,0 +1,144 @@
+package commonweb
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
+)
+
+func getClient(oauthConfig *oauth2.Config, region string) *http.Client {
+	tok, err := tokenFromSecret("gmail-token", region)
+	if err != nil {
+		tok = getTokenFromWeb(oauthConfig)
+		saveTokenToSecret("gmail-token", tok)
+	}
+
+	tokenSource := oauthConfig.TokenSource(context.Background(), tok)
+
+	// Save refreshed token back to Secrets Manager
+	newTok, err := tokenSource.Token()
+	if err == nil && newTok.AccessToken != tok.AccessToken {
+		saveTokenToSecret("gmail-token", newTok)
+	}
+
+	return oauth2.NewClient(context.Background(), tokenSource)
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	config.RedirectURL = "http://localhost:8080"
+
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Println("Opening browser for authorization...")
+
+	codeCh := make(chan string)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		fmt.Fprintln(w, "Authorization successful! You can close this tab.")
+		codeCh <- code
+	})
+
+	go http.ListenAndServe(":8080", nil)
+
+	// Open browser automatically
+	fmt.Printf("Visit this URL if browser doesn't open:\n%v\n", authURL)
+
+	tok, err := config.Exchange(context.TODO(), <-codeCh)
+	if err != nil {
+		log.Fatal("Unable to exchange token:", err)
+	}
+	return tok
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	return tok, json.NewDecoder(f).Decode(tok)
+}
+
+func tokenFromSecret(secretName string, region string) (*oauth2.Token, error) {
+	data, err := GetSecretString(secretName, region)
+	if err != nil {
+		return nil, err
+	}
+	tok := &oauth2.Token{}
+	return tok, json.Unmarshal(data, tok)
+}
+
+func saveTokenToSecret(secretName string, token *oauth2.Token) {
+	data, err := json.Marshal(token)
+	if err != nil {
+		log.Fatal("Could not marshal token:", err)
+	}
+
+	cfg, _ := config.LoadDefaultConfig(context.TODO())
+	client := secretsmanager.NewFromConfig(cfg)
+
+	secretStr := string(data)
+	_, err = client.UpdateSecret(context.TODO(), &secretsmanager.UpdateSecretInput{
+		SecretId:     &secretName,
+		SecretString: &secretStr,
+	})
+	if err != nil {
+		log.Fatal("Could not save token to Secrets Manager:", err)
+	}
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatal("Unable to save token:", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func sendEmail(service *gmail.Service, to, subject, body string) error {
+	raw := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", to, subject, body)
+	encoded := base64.URLEncoding.EncodeToString([]byte(raw))
+	msg := &gmail.Message{Raw: encoded}
+	_, err := service.Users.Messages.Send("me", msg).Do()
+	return err
+}
+
+func SendMail(to string, subject string, body string, region string) {
+	ctx := context.Background()
+
+	// Download credentials.json from Google Cloud Console
+	b, err := GetSecretString("gmail-credentials", region)
+	if err != nil {
+		log.Fatal("Cannot read gmail credentials:", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, gmail.GmailSendScope)
+	if err != nil {
+		log.Fatal("Cannot parse credentials:", err)
+	}
+
+	client := getClient(config, region)
+	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatal("Cannot create Gmail service:", err)
+	}
+
+	err = sendEmail(srv, to, subject, body)
+	if err != nil {
+		log.Fatal("Send failed:", err)
+	}
+	fmt.Println("Email sent!")
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/htstinson/stinsondataapi
 
-go 1.24.11
+go 1.25.8
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.36.3
@@ -12,12 +12,12 @@ require (
 	github.com/gorilla/mux v1.8.1
 	github.com/htstinson/business_searcher v0.0.0-20251230053405-3c3159dfdba1
 	github.com/lib/pq v1.10.9
-	golang.org/x/crypto v0.46.0
+	golang.org/x/crypto v0.51.0
 )
 
 require (
-	cloud.google.com/go/auth v0.12.1 // indirect
-	cloud.google.com/go/auth/oauth2adapt v0.2.7 // indirect
+	cloud.google.com/go/auth v0.20.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
 	cloud.google.com/go/compute/metadata v0.9.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.65 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
@@ -30,28 +30,29 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.17 // indirect
 	github.com/aws/smithy-go v1.22.2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.7 // indirect
-	github.com/googleapis/gax-go/v2 v2.12.4 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.16 // indirect
+	github.com/googleapis/gax-go/v2 v2.22.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
-	go.opentelemetry.io/otel v1.38.0 // indirect
-	go.opentelemetry.io/otel/metric v1.38.0 // indirect
-	go.opentelemetry.io/otel/trace v1.38.0 // indirect
-	golang.org/x/net v0.48.0 // indirect
-	golang.org/x/oauth2 v0.34.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
-	golang.org/x/text v0.32.0 // indirect
-	google.golang.org/api v0.180.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20251022142026-3a174f9686a8 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20251213004720-97cd9d5aeac2 // indirect
-	google.golang.org/grpc v1.77.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.67.0 // indirect
+	go.opentelemetry.io/otel v1.43.0 // indirect
+	go.opentelemetry.io/otel/metric v1.43.0 // indirect
+	go.opentelemetry.io/otel/trace v1.43.0 // indirect
+	golang.org/x/net v0.55.0 // indirect
+	golang.org/x/oauth2 v0.36.0 // indirect
+	golang.org/x/sys v0.45.0 // indirect
+	golang.org/x/text v0.37.0 // indirect
+	google.golang.org/api v0.282.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260319201613-d00831a3d3e7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260523011958-0a33c5d7ca68 // indirect
+	google.golang.org/grpc v1.81.1 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
